77e12d426bfef12402358299afda2364ce3068da
zamarrooo <99203924+zamarrooo@users.noreply.github.com>
2024-07-04 22:36:08 +0100

    test git

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -2,7 +2,7 @@ export default function Player({name, symbol}) {
   return (
     <li>
       <span class="player">
-        <span className="player-name">{name}</span>
+        <span className="player-name">{name}hi</span>
         <span className="player-symbol">{symbol}</span>
       </span>
       <button>Edit</button>
